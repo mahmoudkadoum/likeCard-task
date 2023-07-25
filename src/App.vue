179675a87272b2154c1f4058ef1e8b76b6a75348
mahmoudkadoum <mahmoudkadoum@gmail.com>
2023-07-25 09:07:41 +0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,189 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container nav-container">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg"/>
+      <div class="wrapper">
+        <nav>
+          <div class="nav-links">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Our Products</RouterLink>
+        <div class="dropdown">
+          <button class="dropbtn">Resources</button>
+          <div class="dropdown-content">
+            <a href="#">Resource1</a>
+            <a href="#">Resource2</a>
+            <a href="#">Resource3</a>
+          </div>
+        </div>
+        <RouterLink to="/">Contacts</RouterLink>
+          </div>
+          <div class="nav-btns">
+        <button id="singnUp">Singn Up</button>
+        <button id="logIn">Log In</button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.nav-container {
+  display: flex;
+}
+header {
+  line-height: 1.5;
+  width: 100%;
+  padding-block: 1.25rem;
+  background-color: var(--color-neutral-100, #F8F9FB);
+  border-bottom: 1px solid var(--color-neutral-600, #EAEBF0);
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+}
+
+nav .nav-links , nav .nav-btns {
+  display: flex;
+  align-items: center;
+}
+nav .nav-links {
+  gap: 2rem;
+}
+nav .nav-btns {
+  gap: 1.5rem;
+}
+nav .nav-btns button{
+  border: none;
+  outline: none;
+  font-size: 15px;
+  font-weight: 600;
+  border-radius: 6px;
+  transition: all .25s;
+  cursor: pointer;
+}
+nav .nav-btns button:hover{
+  filter: brightness(.95);
+  transform: scale(1.05);
+}
+nav .nav-btns #singnUp{
+  color: var(--color-primary);
+  background: transparent;
+}
+nav .nav-btns #logIn{
+  padding-inline: 1.125rem;
+  padding-block: 0.75rem;
+  color: #FFFFFF;
+  background: var(--color-primary);
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  text-align: center;
+  line-height: 22px;
+  font-size: 15px;
+  font-weight: 600;
+  color: var(--color-gray-200);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+
+  /* Dropdown Button */
+.dropbtn {
+  background-color: transparent;
+  color: var(--color-gray-200);
+  font-weight: 600;
+  font-size: 15px;
+  border: none;
+  outline: none;
+  position: relative;
+}
+.dropbtn::after {
+  content: '';
+  background-image: url('./assets/arrow-down.svg');
+  position: absolute;
+  right: -25%;
+  top: 25%;
+  background-size: 20px 20px;
+  height: 20px;
+  width: 20px;
+  transform: translate(-25%,-25%);
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #ddd;}
+
+.dropdown:hover .dropdown-content {display: block;}
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    flex-grow: 1;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    font-size: 1rem;
+    padding: 1rem 0;
+  }
+
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,468 @@
+<script setup>
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+import img3 from '../assets/img3.png'
+
+
+import sec4_img1 from '../assets/sec4-img1.png'
+import sec4_img2 from '../assets/sec4-img2.png'
+import sec4_img3 from '../assets/sec4-img3.png'
+import sec4_img4 from '../assets/sec4-img4.png'
+import sec4_img5 from '../assets/sec4-img5.png'
+import sec4_img6 from '../assets/sec4-img6.png'
+
+
+
+
+const sectionTwoCardList = [
+{
+  id:1,
+  src: img1,
+  title: 'Reach your users with new tools',
+  body: 'Efficiently unleash cross-media information without cross-media value. Quickly maximize.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:2,
+  src: img2,
+  title: 'Reach your users with new tools',
+  body: 'Efficiently unleash cross-media information without cross-media value. Quickly maximize.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:3,
+  src: img3,
+  title: 'Reach your users with new tools',
+  body: 'Efficiently unleash cross-media information without cross-media value. Quickly maximize.',
+  extra: '25 Apr 2022 ',
+}
+];
+
+
+const sectionFourCardList = [
+{
+  id:1,
+  src: sec4_img1,
+  title: 'Organize your digital assets with a methodology here and there.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:2,
+  src: sec4_img2,
+  title: 'Organize your digital assets with a methodology here and there.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:3,
+  src: sec4_img3,
+  title: 'Organize your digital assets with a methodology here and there.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:4,
+  src: sec4_img4,
+  title: 'Organize your digital assets with a methodology here and there.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:5,
+  src: sec4_img5,
+  title: 'Organize your digital assets with a methodology here and there.',
+  extra: '25 Apr 2022 ',
+},
+{
+  id:6,
+  src: sec4_img6,
+  title: 'Organize your digital assets with a methodology here and there.',
+  extra: '25 Apr 2022 ',
+}
+];
+</script>
+
+<template>
+  <main>
+    <!-- Section 1 -->
+    <div class="section-1">
+      <p>1% OF THE INDUSTRY</p>
+      <h1>Hype got you here. </h1>
+      <h1>Stay for the support.</h1>
+    </div>
+
+    <!-- Section 2 -->
+    <div class="section-2 container">
+      <div class="first-card card">
+        <img src="../assets/section-2-firstCard.png" alt="" srcset="" loading='lazy'>
+        <div>
+          <h3>Reach your users with new tools. Reach your users with new tools. Reach your users with new tools.</h3>
+          <p>Efficiently unleash cross-media information without cross-media value. Quickly maximize.Efficiently unleash cross-media information without cross-media value. Quickly maximize.Efficiently unleash cross-media.</p>
+          <p>25 Apr 2022 </p>
+        </div>
+      </div>
+      <div class="cards">
+        <div v-for="card in sectionTwoCardList" :key="card.id" class="card">
+          <img :src="card.src" alt="" srcset="" loading='lazy'>
+          <div>
+            <h3>{{card.title}}</h3>
+            <p>{{card.body}}</p>
+            <p>{{card.extra}}</p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Section 3 -->
+    <div class="section-3">
+      <div class="container">
+      <div>
+        <h1>Welcome to your new digital reality that will rock your world.</h1>
+        <p>
+          Let us help you take you from zero to serious business and beyond.
+          Our no-strings attached free trial lets you test our product today.
+        </p>
+      </div>
+      <div>
+        <div style="display: flex;flex-direction: column;gap:.5rem">
+          <div>
+            <input type="text" placeholder="Enter your email">
+            <button>Submit</button>
+          </div>
+          <p>Subscribe to our newsletter</p>
+        </div>
+      </div>
+      </div>
+    </div>
+
+    <!-- Section 4 -->
+    <div class="section-4 container">
+      <h1>Latest Blog Posts</h1>
+      <div class="cards">
+        <div v-for="card in sectionFourCardList" :key="card.id" class="card">
+          <img :src="card.src" alt="" srcset="" loading='lazy'>
+          <div>
+            <p>{{card.extra}}</p>
+            <h3>{{card.title}}</h3>
+            <button>
+              <p>Learn more</p>
+              <img src="../assets/arrow-right.svg" height="20" width="20" alt="">
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Section 5 -->
+    <div class="section-5">
+      <div class="container">
+      <div>
+        <div class="content">
+          <p>1% OF THE INDUSTRY</p>
+          <h1>Welcome to your new digital reality that which will rock your world truly at all.</h1>
+          <div style="display:flex;gap:.5rem">
+            <button>Sign Up</button>
+            <button>Log in</button>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+
+  </main>
+</template>
+
+
+<style scoped>
+.section-1 {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding-inline: 30px;
+  padding-block: 82px;
+  background-color: var(--color-neutral-100, #F8F9FB);
+}
+.section-1 > * {
+  text-align: center;
+}
+.section-1 p {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: -.1px;
+  color: var(--color-primary);
+}
+.section-1 h1 {
+  font-weight: 700;
+  font-size: 52px;
+  line-height: 60px;
+  color: var(--color-gray-200);
+}
+
+.section-2{
+  display: flex;
+  gap: 2.5rem;
+  padding-block: 4rem;
+}
+
+.section-2 .card > div h3 {
+  color: var(--color-gray-200, #272D37);
+}
+.section-2 .card > div p {
+  color: var(--color-gray-50, #4F5B63);
+  letter-spacing: -.1px;
+  line-height: 24px;
+
+}
+.section-2 .card > div p:first-child {
+  font-size: 1rem;
+  font-weight: 400;
+}
+.section-2 .card > div p:nth-child(2) {
+  font-size: 14px;
+  font-weight: 500;
+}
+
+.section-2 .first-card > div {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+.section-2 .cards {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+.section-2 .cards .card {
+  display: flex;
+  gap: 1rem;
+}
+.section-2 .first-card,.cards {
+  flex: 1;
+}
+
+
+
+
+
+.section-3 {
+  background-color: var(--color-primary-200, #2B63D9);
+  padding-block: 4rem;
+}
+
+.section-3 .container {
+  display: flex;
+  gap: 1rem;
+}
+
+.section-3 .container > div {
+  flex: 1;
+}
+
+.section-3 .container > div:first-child{
+display: flex;
+flex-direction: column;
+gap: 24px;
+}
+.section-3 .container > div:first-child h1{
+color: var(--color-base-white, #FFF);
+line-height: 42px;
+font-weight: 600;
+font-size: 2rem;
+}
+.section-3 .container > div:first-child p{
+color: var(--color-primary-50, #B1CCFB);
+}
+
+.section-3 .container div:nth-child(2) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: .5rem;
+}
+
+.section-3 .container div:nth-child(2) div input{
+  background-color: var(--color-base-white, #FFF);
+  border: none;
+  outline: none;
+  padding-block: .75rem;
+  padding-inline: 1rem;
+  width: 20rem;
+  border-bottom-left-radius: 6px;
+  border-top-left-radius: 6px;
+}
+.section-3 .container div:nth-child(2) div button{
+  padding-inline: 1.125rem;
+  padding-block: 0.75rem;
+  color: #FFFFFF;
+  background: var(--color-primary);
+  border-bottom-right-radius: 6px;
+  border-top-right-radius: 6px;
+    border: none;
+  outline: none;
+  cursor: pointer;
+  transition: all .25s;
+}
+.section-3 .container div:nth-child(2) button:hover{
+  filter: brightness(.95);
+}
+.section-3 .container div:nth-child(2) p{
+color: var(--color-primary-50, #B1CCFB);
+font-size: 14px;
+line-height: 20px;
+letter-spacing: -0.1px;
+}
+
+.section-4{
+  display: flex;
+  gap: 2.5rem;
+  padding-block: 4rem;
+  flex-direction: column;
+}
+
+.section-4 > h1 {
+  color: var(--color-gray-200, #272D37);
+  margin-bottom: 24px;
+  width: 100%;
+text-align: center;
+font-size: 32px;
+font-style: normal;
+font-weight: 700;
+line-height: 40px;
+letter-spacing: -0.64px;
+}
+
+.section-4 .cards {
+  display: flex;
+  justify-content: space-between;
+  row-gap: 2rem;
+  flex-wrap: wrap;
+}
+
+.section-4 .cards .card {
+  flex-basis: 31%;
+  width: 31%;
+}
+
+.section-4 .cards .card > img{
+  width: 100%;
+}
+
+.section-4 .cards .card h3{
+color: var(--color-gray-200, #272D37);
+font-size: 22px;
+font-weight: 600;
+line-height: 30px;
+letter-spacing: -0.22px;
+  padding: 0§;
+}
+
+.section-4 .cards .card p{
+font-size: 14px;
+font-weight: 500;
+line-height: 20px;
+letter-spacing: -0.1px;
+color: var(--color-gray-50, #5F6D7E);
+}
+.section-4 .cards .card button{
+  display: flex;
+  width: 100%;
+  gap: .25rem;
+  align-items: center;
+  background: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: all .25s;
+  padding: 0;
+}
+
+.section-4 .cards .card button p{
+  color: var(--color-primary, #437EF7);
+  font-weight: 600;
+}
+
+.section-5 {
+    background-color: var(--color-primary-200, #2B63D9);
+  padding-block: 4rem;
+}
+
+.section-5 .container > div {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.section-5 .container .content{
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.section-5 .container .content p{
+  text-align: center;
+  width: 100%;
+  color: var(--color-base-white, #FFF);
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  letter-spacing: -0.1px;
+}
+.section-5 .container .content h1{
+  text-align: center;
+  width: 100%;
+  color: var(--color-base-white, #FFF);
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 42px;
+  letter-spacing: -0.32px;
+  margin-bottom: 2rem;
+}
+.section-5 .container .content div{
+  justify-content: center;
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+}
+.section-5 .container .content div button{
+  padding-inline: 1.125rem;
+  padding-block: 0.75rem;
+  border-radius: 6px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  transition: all .25s;
+}
+.section-5 .container .content div button{
+  filter: brightness(.95);
+}
+  .section-5 .container .content div button:first-of-type{
+    background: #FFFFFF;
+    color: var(--color-primary);
+  }
+  .section-5 .container .content div button:nth-child(2){
+    color: #FFFFFF;
+    background: var(--color-primary);
+  }
+
+  @media (min-width: 1024px) {
+  .section-1::before {
+  transform: translateX(-50%);
+  left: 50%;
+  }
+  }
+  @media (min-width: 768px) and (max-width: 1023px) {
+  .section-1::before {
+  transform: translateX(-0%);
+}
+}
+  @media (min-width: 600px) and (max-width: 767px) {
+  .section-1::before {
+  transform: translateX(-0%);
+}
+}
+  @media (max-width: 600px) {
+  .section-1::before {
+  transform: translateX(-0%);
+}
+}
+
+</style>
